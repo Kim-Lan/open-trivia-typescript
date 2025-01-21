a34c29926bb4b0c6d6fbeb798dcfd492d13bd545
Kim-Lan <me@kim-lan.com>
2025-01-20 16:01:31 -0800

    Add basic logic

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import './css/style.css'
+import { shuffleArray } from './util.ts'
+
+let currentCorrectAnswer: string = '';
 
 function init(): void {
   fetchNewQuestion();
@@ -9,6 +12,40 @@ function renderQuestion(question: string): void {
   questionElement.innerHTML = question;
 }
 
+function renderChoices(correctAnswer: string, incorrectAnswers: string[]): void {
+  currentCorrectAnswer = correctAnswer;
+
+  let choices: string[] = [correctAnswer, ...incorrectAnswers];
+  choices = shuffleArray(choices);
+  
+  const choicesElement = document.getElementById('choices') as HTMLUListElement;
+  choicesElement.innerHTML = '';
+
+  choices.forEach(choice => {
+    const liElement = document.createElement('li') as HTMLLIElement;
+    const buttonElement = document.createElement('button') as HTMLButtonElement;
+    buttonElement.innerHTML = choice;
+
+    buttonElement.addEventListener('click', onChoiceSelected);
+
+    liElement.appendChild(buttonElement);
+    choicesElement.appendChild(liElement);
+  });
+}
+
+function onChoiceSelected(event: Event): void {
+  const target: HTMLElement = event.target;
+  const selectedChoice: string = target.textContent;
+  
+  if (selectedChoice === currentCorrectAnswer) {
+    target.classList.add('correct');
+    console.log('Correct!');
+  } else {
+    target.classList.add('incorrect');
+    console.log('Incorrect');
+  }
+}
+
 function fetchNewQuestion(): void {
   fetch('https://opentdb.com/api.php?amount=1&type=multiple')
     .then(function (response) {
@@ -17,6 +54,7 @@ function fetchNewQuestion(): void {
     .then(function (data) {
       console.log(data.results[0]);
       renderQuestion(data.results[0].question);
+      renderChoices(data.results[0].correct_answer, data.results[0].incorrect_answers);
     });
 }
 
